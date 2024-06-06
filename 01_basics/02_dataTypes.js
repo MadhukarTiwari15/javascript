752d735816b2eb4a7ddb2684183752edb783b574
Madhukar Tiwari <143827855+MadhukarTiwari15@users.noreply.github.com>
2024-06-06 11:56:20 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";  // treat all code as new versions
+ 
+//alert(3+3)    // we are using nodejs, not browser
+
+console.log( 3 +
+    3                        // code readability should be high
+)
+
+//data types
+
+// number => 2 to power 5
+// bigint   // use when there is a very large number 
+// string => "madhukr"
+// Boolean => true/false
+// null => standaloneValue
+// undefined => 
+
+
+    //object
+
+console.log(typeof undefined)    // undefined
+console.log(typeof null)         // object
