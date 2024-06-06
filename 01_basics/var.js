3331a4ec1a5307c7e83cf5d0a09c3d7bc5f97e0a
Madhukar Tiwari <143827855+MadhukarTiwari15@users.noreply.github.com>
2024-06-06 11:35:23 +0000

    studied variabled

---
+++ 01_basics/var.js
@@ -0,0 +1,18 @@
+const number = 78
+var city = "anuppur"
+let name = "madhukar"
+surname = "tiwari"
+let accountState;     // will be undefined
+
+//number = 87     // const variabels can not be changed
+console.log(number)
+city = "shahdol"
+name = "harsh"
+surname = "pandey"
+
+console.table([name, number, city,surname,accountState])
+
+/* 
+we don't use var because of issue of block and functional scope
+*/
+
