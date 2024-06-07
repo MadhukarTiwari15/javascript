01c42b9a03cd09c722614c1e6565da155fd582fd
Madhukar Tiwari <143827855+MadhukarTiwari15@users.noreply.github.com>
2024-06-07 07:47:17 +0000

    understand the diifference  between operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log( 2 > 1)
+// console.log( 2 >=  1)
+// console.log( 2 != 1)
+// console.log( 2 == 1)
+
+
+// javascript convert the data type of one value  to another data type to compare 
+console.log ("2" > 1)
+console.log("02" > 1)
+console.log(null == 0)      // null means absence, when compare js convert it into int for 0(int) but it can be converted so we receive false
+console.log(null > 0)
+// // equity check == and comparison< >= work differently
+// comparison operator treat null an 0 so we receive null > 0 false and null >= 0 true
+console.log(null >= 0)
+
+
+//so in simple when we use comparison operator then coercion happens and not for equity 
+
+console.log()
