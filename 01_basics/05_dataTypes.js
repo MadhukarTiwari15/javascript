6a5ea7bf0599422bae54e57a551047eca8549176
Madhukar Tiwari <143827855+MadhukarTiwari15@users.noreply.github.com>
2024-06-07 08:19:28 +0000

    updated data types

---
+++ 01_basics/05_dataTypes.js
@@ -0,0 +1,27 @@
+//  (call by value) primitive
+
+// 7 types - String, Number, Boolean , BigInt , Null , Undefinde , Symbol
+
+const bigNumber = 983457290834750293485n
+
+const id = Symbol('124')
+
+// refrence type ( non - primitive)
+// objects, Array, Functions
+
+const heros = ["shaktiman","ironman"];
+
+//object
+let myObj = {
+    name: "madhukar",
+    age: 123
+}
+
+
+const fun = function(){
+    console.log("hello")
+}
+
+// JavaScript is a dynamically typed language, which means that data types of
+//  variables are determined by the value they hold at runtime and can change 
+//  throughout the program as we assign different values to them
