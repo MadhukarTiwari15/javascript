fb3893ce07eb32abcc737f96890ae7055f80c095
Madhukar Tiwari <madhukar.tiwari@unthinkable.cogit config --global user.email madhukar.tiwari@unthinkable.cogit config --global user.email madhukar.tiwari@unthinkable.co>
2024-06-07 14:31:35 +0530

    memory details

--- 01_basics/05_dataTypes.js
+++ 01_basics/05_dataTypes.js
@@ -18,10 +18,23 @@ let myObj = {
 }
 
 
-const fun = function(){
-    console.log("hello")
-}
+// const fun = function(){
+//     console.log("hello")
+// }
 
 // JavaScript is a dynamically typed language, which means that data types of
 //  variables are determined by the value they hold at runtime and can change 
 //  throughout the program as we assign different values to them
+
+// primitive - stack 
+// refrence - heap ( here the object take refrence, for )
+
+// ex :
+let user1 = {
+    name: "madhuakr",
+    email: "tiwari@gmail.com"
+}
+
+let user2 =  user1
+user2.email = "madhukar@gmail"
+console.log(user1.email)
