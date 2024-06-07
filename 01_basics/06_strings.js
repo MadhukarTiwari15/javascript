3cbbba3002001082be5c993127f3893e81da9e58
Madhukar Tiwari <madhukar.tiwari@unthinkable.cogit config --global user.email madhukar.tiwari@unthinkable.cogit config --global user.email madhukar.tiwari@unthinkable.co>
2024-06-07 23:06:58 +0530

    methods of string

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -8,3 +8,16 @@ console.log(`hello ${str} how are you`)
 
 const name = new String('tiwari')
 console.log (name[0])
+
+let str = new Sring {'madhukar'}
+0: "m"
+1: "a"
+2: "d"
+3: "h"
+4: "u"
+5: "k"
+6: "a"
+7: "r"
+length: 8
+[[Prototype]]: Stringanchor: 
+ƒ anchor()at: ƒ at()big: ƒ big()blink: ƒ blink()bold: ƒ bold()charAt: ƒ charAt()charCodeAt: ƒ charCodeAt()codePointAt: ƒ codePointAt()concat: ƒ concat()constructor: ƒ String()endsWith: ƒ endsWith()fixed: ƒ fixed()fontcolor: ƒ fontcolor()fontsize: ƒ fontsize()includes: ƒ includes()indexOf: ƒ indexOf()isWellFormed: ƒ isWellFormed()italics: ƒ italics()lastIndexOf: ƒ lastIndexOf()length: 0link: ƒ link()localeCompare: ƒ localeCompare()match: ƒ match()matchAll: ƒ matchAll()normalize: ƒ normalize()padEnd: ƒ padEnd()padStart: ƒ padStart()repeat: ƒ repeat()replace: ƒ replace()replaceAll: ƒ replaceAll()search: ƒ search()slice: ƒ slice()small: ƒ small()split: ƒ split()startsWith: ƒ startsWith()strike: ƒ strike()sub: ƒ sub()substr: ƒ substr()substring: ƒ substring()sup: ƒ sup()toLocaleLowerCase: ƒ toLocaleLowerCase()toLocaleUpperCase: ƒ toLocaleUpperCase()toLowerCase: ƒ toLowerCase()toString: ƒ toString()toUpperCase: ƒ toUpperCase()toWellFormed: ƒ toWellFormed()trim: ƒ trim()trimEnd: ƒ trimEnd()trimLeft: ƒ trimStart()trimRight: ƒ trimEnd()trimStart: ƒ trimStart()valueOf: ƒ valueOf()Symbol(Symbol.iterator): ƒ [Symbol.iterator]()[[Prototype]]: Object[[PrimitiveValue]]: ""[[PrimitiveValue]]: "madhukar"
