a31f0e766c8af30f114321f746345ab093013c63
Madhukar Tiwari <madhukar.tiwari@unthinkable.cogit config --global user.email madhukar.tiwari@unthinkable.cogit config --global user.email madhukar.tiwari@unthinkable.co>
2024-06-07 18:01:56 +0530

    string

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,10 @@
+let str = "madhukar";
+console.log (str)
+console.log(str[0])
+console.log(str + "madhukar")
+console.log(`hello ${str} how are you`)
+
+// 2nd way
+
+const name = new String('tiwari')
+console.log (name[0])
