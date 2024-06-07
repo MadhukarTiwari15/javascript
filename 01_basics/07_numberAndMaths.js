a1125d36296fea0da7c31e31184713cb1b32ebc2
Madhukar Tiwari <madhukar.tiwari@unthinkable.cogit config --global user.email madhukar.tiwari@unthinkable.cogit config --global user.email madhukar.tiwari@unthinkable.co>
2024-06-07 23:51:42 +0530

    added maths and numbers

---
+++ 01_basics/07_numberAndMaths.js
@@ -0,0 +1,57 @@
+// const score = 100
+// const num = new Number(100)
+// console.log(score)
+// console.log(num)
+
+// Number {78}
+// [[Prototype]]
+// : 
+// Number
+// constructor
+// : 
+// ƒ Number()
+// toExponential
+// : 
+// ƒ toExponential()
+// toFixed
+// : 
+// ƒ toFixed()
+// toLocaleString
+// : 
+// ƒ toLocaleString()
+// toPrecision
+// : 
+// ƒ toPrecision()
+// toString
+// : 
+// ƒ toString()
+// valueOf
+// : 
+// ƒ valueOf()
+// [[Prototype]]
+// : 
+// Object
+// [[PrimitiveValue]]
+// : 
+// 0
+// [[PrimitiveValue]]
+// : 
+// 78
+
+
+
+
+//   maths
+
+// console.log(Math)
+// console.log(Math.abs(-98))
+// console.log(Math.round(9.7))
+// console.log(Math.ceil(7.3))
+// console.log(Math.floor(8.9))
+
+console.log(Math.floor(Math.random() * 10) + 1)
+
+let max = 20
+let min = 10
+
+console.log(Math.floor(Math.random() * 10) + min)
